cc27a9bb482fe2193ddf7a52c3a1251eec1000be
dumanet <mniccolo8@gmail.com>
2025-02-11 19:23:41 -0400

    Actualizacion

--- Practica3.c
+++ Practica3.c
@@ -4,32 +4,61 @@
 #BYTE PORTD = 8
 #BYTE PORTA = 5
 #BYTE PORTB = 6
+#DEFINE U PORTA, 0
+#DEFINE D PORTA, 1
 Byte CONST display[10] = {0x3f, 0x06, 0x5b, 0x4f, 0x66,
                           0x6d, 0x7d, 0x07, 0x7f, 0x67};
-#DEFINE TUNI PORTD, 7
-#DEFINE TDEC PORTD, 3
-INT CONT;
+int times, sec, uni, dec, i;
 
-LONG CONCRET;
-VOID MOSTRAR() {
-  INT UNI, DEC;
+#int_rtcc clock() {
+times--;
+set_rtcc(238);
 
-  DEC = CONT / 10;
-  UNI = CONT % 10;
-  PORTB = (DISPLAY[UNI]);
-
-  BIT_CLEAR(TDEC);
-  BIT_SET(TUNI);
-  DELAY_MS(1);
-  PORTB = (DISPLAY[DEC]);
-
-  BIT_CLEAR(TUNI);
-  BIT_SET(TDEC);
-  DELAY_MS(1);
+if (times == 0) {
+  sec++;
+  times = 217;
+}
 }
 
-VOID RET1SEG() {
-  CONTRET = 500;
-  WHILE(CONTRET > 0) {
+void show() {
+  i = sec; // Tentativo hasta la linea 30.
+  uni = 0;
+  dec = 0;
 
-    MOSTRAR();
+  while (i >= 10) {
+    i = i - 10;
+    dec++; // Hasta aca!
+  }
+  uni = i;
+
+  PORTC = display[uni];
+  bit_set(U);
+  bit_clear(D);
+  delay_ms(1);
+
+  PORTC = display[dec];
+  bit_set(D);
+  bit_clear(U);
+  delay_ms(1);
+}
+
+int main() {
+  SET_TRIS_B(0);
+  SET_TRIS_A(0);          // Chequear si es entrada o salida!
+  SET_TRIS_C(0B11111111); // ENTRADA BJT
+  uni = 0;
+  times = 217;
+  sec = 0;
+
+  set_rtcc(238);
+  setup_counters(rtcc_internal, rtcc_div_256);
+  enable_interrupts(int_rtcc);
+  enable_interrupts(global);
+
+  while (1) {
+    if (sec == 60)
+      sec = 0;
+    else
+      show();
+  }
+}
