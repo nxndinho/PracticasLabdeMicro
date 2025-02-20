23eca65721f3cb38d6ac3eb156b08c541d060123
duma <mniccolo8@gmail.com>
2025-02-20 06:57:15 +0000

    Update practica4.c

--- practica4.c
+++ practica4.c
@@ -1,9 +1,21 @@
 #include <16f877a.h>
 #include <lcd.c>
-//#include <timer.h>
-#fuses xt, noprotect, nowdt, nobrownout, put, nolvp, wrt
+#include <stdio.h>
+#use delay(clock=4000000)
+#define USE_PORTB_LCD TRUE	//Habilita uso en PORTB para LCD
+#fuses xt, noprotect, nowdt, nobrownout, put, nolvp
 #BYTE PORTA = 5
 #BYTE PORTB = 6
 #BYTE PORTD = 8
 
-		
+#define pulse_0 PIN_A0
+#define pulse_1 PIN_A1
+#define pulse_2 PIN_A2
+#define pulse_3 PIN_A3
+
+int second, minute, hour, day, month, year;
+
+int main(){
+	lcd_init();
+	lcd_putc("\f");
+}
